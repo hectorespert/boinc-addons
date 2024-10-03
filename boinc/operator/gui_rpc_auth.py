f25823d0e5960ccde242ac8def15f85d6c002b74
Hector Espert <hectorespertpardo@gmail.com>
2024-10-03 18:45:45 +0000

    Refactor operator

---
+++ boinc/operator/gui_rpc_auth.py
@@ -0,0 +1,13 @@
+import logging
+import os
+from venv import logger
+
+def prepare_gui_rpc_auth(data_folder: str, password: str | None) -> None:
+    gui_rpc_auth = f'{data_folder}/gui_rpc_auth.cfg'
+    if os.path.exists(gui_rpc_auth):
+        os.remove(gui_rpc_auth)
+        logger.debug(f'Removing existing BOINC GUI RPC auth file')
+    if password:
+        with open(gui_rpc_auth, 'w') as f:
+            f.write(password)
+            logging.debug(f'Writing BOINC GUI RPC auth file on {gui_rpc_auth}')
